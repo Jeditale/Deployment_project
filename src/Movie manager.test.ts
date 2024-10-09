bad5ccaef5b9f54736a25d5b187f1bcfe4580d38
Jeditale <69350223+Jeditale@users.noreply.github.com>
2024-10-09 22:14:19 +0700

    Update Movie manager.test.ts

--- src/Movie manager.test.ts
+++ src/Movie manager.test.ts
@@ -11,7 +11,7 @@ describe('MovieManager', () => {
   });
 
   test('should add a movie to the collection', () => {
-    movieManager.addMovie(movie1);
+    movieManager.addMovie(movie2);
     expect(movieManager.getMovies()).toContain(movie1);
   });
 
